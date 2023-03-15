19ee04f935ea8eb019785f1e9b605449e7532a2c
MeNoobly <84956743974@mail.ru>
2023-03-15 19:25:44 +0300

    end 6 lessons

--- src/index.js
+++ src/index.js
@@ -2,25 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import App from './App';
-import { configureStore } from '@reduxjs/toolkit';
-
-// const action = {type: "", payload: ""}
-const defaultState = {
-    cash: 0,
-}
-
-const reducer = (state = defaultState, action) => {
-    switch(action.type) {
-        case "ADD_CASH":
-            return {...state, cash: state.cash + action.payload}
-        case "GET_CASH":
-            return {...state, cash: state.cash - action.payload}
-        default:
-            return state;
-    }
-}
-
-const store = configureStore({reducer: reducer});
+import { store } from './store';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
---
+++ src/store/index.js
@@ -0,0 +1,12 @@
+import { applyMiddleware, combineReducers, configureStore } from "@reduxjs/toolkit";
+import { composeWithDevTools } from "redux-devtools-extension";
+import thunk from "redux-thunk";
+import { cashReducer } from "./cashReducer";
+import { customerReducer } from "./customerReducer";
+
+const rootReducer = combineReducers({
+    cash: cashReducer,
+    customers: customerReducer,
+})
+
+export const store = configureStore({reducer: rootReducer, devTools: composeWithDevTools(applyMiddleware(thunk))});
